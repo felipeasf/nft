f03b9b49f5f6485a2104852703a4d019bf8967c9
joaoocb <joaoocb@gmail.com>
2018-12-13 14:25:24 -0200

    Adding example of game contract

---
+++ include/example/game.hpp
@@ -0,0 +1,32 @@
+/**
+*  @file game.hpp
+*/
+
+#include <eosiolib/eosio.hpp>
+#include <eosiolib/asset.hpp>
+
+using namespace eosio;
+
+namespace example {
+
+    CONTRACT game : public contract {
+        public:
+            /**
+             * @brief Construct a game object.
+             * @param recvr - The name of this contract.
+             * @param cd - The code name of the action this contract is processing.
+             * @param ds - The datastream used.
+             */
+            game(name recvr, name code, datastream<const char*> ds) : contract(recvr, code, ds) {}
+
+            /**
+             * @brief Transfer token between accounts.
+             * @param from - Account from where token will be transfered.
+             * @param to - Account where token will be transfered to.
+             * @param sym - Token symbol.
+             * @param tk_id - Token id.
+             * @param memo - Transfer memo.
+             */
+            ACTION transfer(name from, name to, symbol sym, uint64_t tk_id, std::string memo);
+   };
+} //example namespace
---
+++ src/game.cpp
@@ -0,0 +1,10 @@
+/**
+*  @file game.cpp
+*/
+
+#include "example/game.hpp"
+
+void example::game::transfer(name from, name to, symbol sym, uint64_t tk_id, std::string memo) {}
+
+EOSIO_DISPATCH(example::game, (transfer))
+ 
