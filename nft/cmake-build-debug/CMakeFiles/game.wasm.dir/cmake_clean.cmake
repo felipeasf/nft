20266b00afe2c0a8e1107c99e6e7d9712b25b84a
Felipe Siecola <felipe@Felipes-MacBook-Pro.local>
2019-01-06 17:35:42 -0200

    nft files

---
+++ nft/cmake-build-debug/CMakeFiles/game.wasm.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/game.wasm.dir/src/game.cpp.o"
+  "game.wasm.pdb"
+  "game.wasm"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/game.wasm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
